eb5855617615f72f8df4c7c4ee03ad4d7bce490d
Kurt Nelson <kurtisnelson@gmail.com>
2015-06-30 10:20:19 -0500

    Swapcase

--- w01/tue/lecture/spec.rb
+++ w01/tue/lecture/spec.rb
@@ -1,10 +1,27 @@
 require_relative 'strings'
 
+STRING0 = "[thisisateststringz]"
+STRING1 = "{this is a test stringZ}"
+STRING2 = "This is a test stringZ"
+STRING3 = "THIS IS a TEST STRINGz"
+
 RSpec.describe "#upcase" do
-  STRING0 = "thisisateststring"
   it { expect(upcase(STRING0)).to eq STRING0.upcase}
-  STRING1 = "this is a test string"
   it { expect(upcase(STRING1)).to eq STRING1.upcase}
-  STRING2 = "This is a test string"
   it { expect(upcase(STRING2)).to eq STRING2.upcase}
+  it { expect(upcase(STRING3)).to eq STRING3.upcase}
+end
+
+RSpec.describe "#downcase" do
+  it { expect(downcase(STRING0)).to eq STRING0.downcase}
+  it { expect(downcase(STRING1)).to eq STRING1.downcase}
+  it { expect(downcase(STRING2)).to eq STRING2.downcase}
+  it { expect(downcase(STRING3)).to eq STRING3.downcase}
+end
+
+RSpec.describe "#swapcase" do
+  it { expect(swapcase(STRING0)).to eq STRING0.swapcase}
+  it { expect(swapcase(STRING1)).to eq STRING1.swapcase}
+  it { expect(swapcase(STRING2)).to eq STRING2.swapcase}
+  it { expect(swapcase(STRING3)).to eq STRING3.swapcase}
 end
--- w01/tue/lecture/strings.rb
+++ w01/tue/lecture/strings.rb
@@ -1,8 +1,8 @@
 def upcase str
   upper = ""
   str.each_char do |c|
-    if c.ord < 122 && c.ord >= 97
-     upper << (c.ord - 32).chr
+    if c.ord <= 122 && c.ord >= 97
+      upper << (c.ord - 32).chr
     else
       upper << c
     end
@@ -10,4 +10,28 @@ def upcase str
   upper
 end
 
-#puts upcase "stuff"
+def downcase str
+  down = ""
+  str.each_char do |c|
+    if c.ord <= 90 && c.ord >= 65
+      down << (c.ord + 32).chr
+    else
+      down << c
+    end
+  end
+  down 
+end
+
+def swapcase str
+  swap = ""
+  str.each_char do |c|
+    if c.ord <= 122 && c.ord >= 97
+      swap << (c.ord - 32).chr
+    elsif c.ord <= 90 && c.ord >= 65
+      swap << (c.ord + 32).chr
+    else 
+      swap << c
+    end
+  end
+  swap
+end
