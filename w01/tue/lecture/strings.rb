c235e2adff65ff45719ec088e9b1bc6a35baf0c6
Kurt Nelson <kurtisnelson@gmail.com>
2015-06-30 09:55:12 -0500

    Fix upcase

--- w01/tue/lecture/strings.rb
+++ w01/tue/lecture/strings.rb
@@ -1,7 +1,11 @@
 def upcase str
   upper = ""
   str.each_char do |c|
-    upper << (c.ord - 32).chr
+    if c.ord < 122 && c.ord >= 97
+     upper << (c.ord - 32).chr
+    else
+      upper << c
+    end
   end
   upper
 end
