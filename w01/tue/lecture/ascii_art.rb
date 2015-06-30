14cda87fb9f840acd886d53988b44cc4be8dccc7
Kurt Nelson <kurtisnelson@gmail.com>
2015-06-30 13:29:13 -0500

    Improved ASCII table

--- w01/tue/lecture/ascii_art.rb
+++ w01/tue/lecture/ascii_art.rb
@@ -1,10 +1,19 @@
 #-----------------------------------
 #| Name |  Favorite Food  |  Grade |
 #===================================
-#| Ryan |  stuff          |   F    |
-#| Ben  |  meat           |   A-   |
-#| Luke |  ????           |   Z    |
+#| Ryan |  stuff          |   0    |
+#| Ben  |  meat           |   99   |
+#| Luke |  ????           |   -5   |
+#===================================
+#| Grade | 30                      |
 #-----------------------------------
+data = {}
+data[:ryan] = {:food => "stuff", grade: 0}
+data[:ben]  = {:food => "meat", grade: 99}
+data[:luke] = {food: "???", grade: -5}
+sum = 0
+data.each { |name, value| sum += value[:grade] }
+average = sum / data.length
 
 def horizontal_line len
   str = ""
@@ -25,10 +34,6 @@ def pretty_data name, value, len
   base << " " while base.length < len - 1
   base << "|"
 end
-data = {}
-data[:ryan] = {food: "stuff", grade: 0}
-data[:ben]  = {food: "meat", grade: 99}
-data[:luke] = {food: "???", grade: -5}
 
 hdr = header "Name", "Favorite Food", "Grade"
 puts horizontal_line(hdr.length)
@@ -37,4 +42,6 @@ puts double_line(hdr.length)
 data.each do |name, value|
   puts pretty_data(name, value, hdr.length)
 end
+puts double_line(hdr.length)
+puts header "Grade", average
 puts horizontal_line(hdr.length)
